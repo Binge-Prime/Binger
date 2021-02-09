02c666fdecf9336eab8d90bfef8746af61f839b6
Kestrel Gorlick <kestrel.gorlick@gmail.com>
2021-02-08 19:21:45 -0700

    Added AllProducts Component, tied it to redux, and updated component filenames.

---
+++ client/components/AllProducts.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Component } from 'react';
+import { fetchProducts } from '../store/products'
+
+
+class AllProducts extends Component {
+    componentDidMount () {
+        this.props.init();
+    }
+    render () {
+        const { products } = this.props;
+
+        return (
+            <div>
+                <ul>
+                    {
+                        products.map( (product) => {
+                            return (
+                                <li key={ product.id }>
+                                    <ul>
+                                        <li> { product.name } </li>
+                                        <li> ${ product.price } </li>
+                                        {/* <li> { product.avgRating } </li> */}
+                                    </ul>
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => ({ products: state.products.products });
+
+const mapDispatchToProps = (dispatch) => ({
+        init: () => dispatch(fetchProducts()),
+})
+
+export default connect(mapStateToProps, mapDispatchToProps)(AllProducts);
