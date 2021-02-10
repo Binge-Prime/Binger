e6454d0d7db44dfc5d96055f6c616961e0932b90
serenajiao0624 <serena.yifei.j@gmail.com>
2021-02-09 19:00:09 -0500

    adding routs

--- server/api/products.js
+++ server/api/products.js
@@ -1,12 +1,58 @@
 const router = require('express').Router()
-const { models: { Product }} = require('../db')
+const { models: { Product, Review }} = require('../db')
 module.exports = router
 
 router.get('/', async (req, res, next) => {
   try {
     const products = await Product.findAll()
     res.json(products)
-  } catch (err) {
+  } 
+  catch (err) {
     next(err)
+  }
+})
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.send(await Product.findAll({
+      include: [Review],
+      where:{
+          id:req.params.id
+      }
+  }));
+  } 
+  catch (err) {
+    next(err)
+  }
+})
+
+router.put('/update/:id',async(req,res,next)=>{
+  try {
+    const product = await Product.findByPk(req.params.id)
+    res.send(await product.update(req.body))
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
+router.post('/create',async(req,res,next)=>{
+  try {
+    const newProduct = await Product.create(req.body)
+    res.status(201).send(newProduct)
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
+router.delete('/delete/:id',async(req,res,next)=>{
+  try{
+      const product = await Product.findByPk(req.params.id)
+      await product.destroy()
+      res.send(product)
+  }
+  catch(error){
+      next(error)
   }
 })
--- server/api/users.js
+++ server/api/users.js
@@ -15,3 +15,47 @@ router.get('/', async (req, res, next) => {
     next(err)
   }
 })
+
+router.get('/:id', async (req, res, next) => {
+  try {
+    res.send(await User.findAll({
+      where:{
+          id:req.params.id
+      }
+  }));
+  } 
+  catch (err) {
+    next(err)
+  }
+})
+
+router.put('/update/:id',async(req,res,next)=>{
+  try {
+    const user = await User.findByPk(req.params.id)
+    res.send(await user.update(req.body))
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
+router.post('/create',async(req,res,next)=>{
+  try {
+    const newUser = await User.create(req.body)
+    res.status(201).send(newUser)
+  }
+  catch(error) {
+    next(error)
+  }
+})
+
+router.delete('/delete/:id',async(req,res,next)=>{
+  try{
+      const user = await User.findByPk(req.params.id)
+      await user.destroy()
+      res.send(user)
+  }
+  catch(error){
+      next(error)
+  }
+})
