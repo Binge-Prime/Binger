e0076b01f4d0c334411824b49a9dc36b6f414f86
roman shteynberg <rys495@gmail.com>
2021-02-20 12:41:06 -0500

    presented version

--- client/components/index.js
+++ client/components/index.js
@@ -3,7 +3,7 @@
  * This way, we can import all of our components from the same place, rather than
  * having to figure out which file they belong to!
  */
-export {default as Navbar} from './Navbar'
+export { default as Navbar } from './naviagtion'
 export {default as Home} from './Home'
 export { Login, Signup } from './AuthForm'
 export { default as AdminTools } from './AdminTools'
